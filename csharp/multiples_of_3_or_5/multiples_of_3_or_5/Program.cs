dbcbc73140ee70a610ebbe580e0f626d6ea75818
James Thach <j.thach000@gmail.com>
2022-12-19 23:13:14 -0500

    Add first completed problem

---
+++ csharp/multiples_of_3_or_5/multiples_of_3_or_5/Program.cs
@@ -0,0 +1,14 @@
+﻿/* TODO:
+ * [x] variable holding sum (can't access sum variable inside loop due to scope)
+ * [x] loop iterating through the numbers [1, 1000)
+ * [x] print sum
+ */
+
+using Internal;
+
+int sum = 0;
+for (int i = 1; i < 1000; i++)
+{
+    if (i % 3 == 0 || i % 5 == 0) sum += i;
+}
+Console.WriteLine(sum);
